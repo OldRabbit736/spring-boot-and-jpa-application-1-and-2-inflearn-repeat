6fb3bd061208708f90576395b38faf238e5e7b00
OldRabbit736 <sylvan0212@gmail.com>
2022-07-06 16:05:38 +0900

    완료: 회원 기능 테스트

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional  // rollback을 위해
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void member_join() {
+        Member member = new Member();
+        member.setName("kim");
+
+        Long savedId = memberService.join(member);
+
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    public void duplicate_member_join_exception() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim1");
+
+        Member member2 = new Member();
+        member2.setName("kim1");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        assertThrows(IllegalStateException.class, () -> {
+            memberService.join(member2);
+        });
+    }
+}
