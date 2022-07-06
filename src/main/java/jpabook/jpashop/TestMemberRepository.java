7bd9fcfb62f4d79f7715da4b418d3c9abd055882
OldRabbit736 <sylvan0212@gmail.com>
2022-07-06 13:08:00 +0900

    완료: 회원 서비스 개발

--- src/main/java/jpabook/jpashop/TestMemberRepository.java
+++ src/main/java/jpabook/jpashop/TestMemberRepository.java
@@ -2,13 +2,12 @@ package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Member;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @Repository
-public class MemberRepository {
+public class TestMemberRepository {
 
     @PersistenceContext
     private EntityManager em;
--- src/test/java/jpabook/jpashop/TestMemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/TestMemberRepositoryTest.java
@@ -6,12 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
-class MemberRepositoryTest {
+class TestMemberRepositoryTest {
 
-    @Autowired MemberRepository memberRepository;
+    @Autowired
+    TestMemberRepository memberRepository;
 
     @Test
     @Transactional
