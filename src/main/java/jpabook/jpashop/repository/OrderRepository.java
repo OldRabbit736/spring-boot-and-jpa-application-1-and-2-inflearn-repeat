d0ad39bebe972eda8f31f0caff984a2315118c25
OldRabbit736 <sylvan0212@gmail.com>
2022-07-07 22:40:59 +0900

    완료: 주문 검색 기능 개발

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -21,6 +21,21 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    //public List<Order> findAll(OrderSearch orderSearch) {    }
+    // 동적 쿼리... OrderSearch에서 orderStatus나 memberName이 null인 경우
+    // 쿼리 스트링 자체가 변경되어야 한다... where 절이 없어져야 하는 것이다.
+    // 그래서 동적 쿼리이다.
+    // 동적 쿼리 중에 JPA Criteria가 있는데... 실무에서는 잘 쓰이지 않는다고 한다. 너무 복잡함!
+    // 이것을 해결한 라이브러리가 QueryDSL이다!
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery(
+                "select o from Order o join o.member m" +
+                        " where o.status = :status" +
+                        " and m.name like :name"
+                , Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000)    // 최대 1000건
+                .getResultList();
+    }
 
 }
